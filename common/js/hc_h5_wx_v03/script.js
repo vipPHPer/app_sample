5aad07d21b7c8040d301c273b97c06292a06ff86
chenfuxin <since1991_vip@163.com>
2017-07-18 17:22:31 +0800

    update

--- common/js/hc_h5_wx_v03/script.js
+++ common/js/hc_h5_wx_v03/script.js
@@ -4,21 +4,21 @@
  * @date    2016-11-02 09:14:33
  */
 
-var apiDetailUrl = '/controller/hc_h5_wx_v02/get_article_detail.json';
+var apiArticle_detailUrl = '/controller/hc_h5_wx_v03/get_article_detail.json';
 var _common = {
   //ajax请求
-  sendAjax: function(url, params, dataType, callback) {
+  sendAjax: function(url, dataType, callback) {
     $.ajax({
       url: url,
-      data: params,
       dataType: dataType,
       success: callback
     });
   }
 };
 
-_common.sendAjax(apiDetailUrl, {}, 'json', function(data) {
+_common.sendAjax(apiArticle_detailUrl, 'json', function(data) {
   if (data && data.code === 0) {
+    console.log(data.data);
     window.vm = new Vue({
       el: '#app',
       data: {
