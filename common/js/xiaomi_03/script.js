357e6518d7858b3e34df491bb16f73183de093cb
chenfuxin <since1991_vip@163.com>
2017-07-19 10:54:18 +0800

    update

--- common/js/xiaomi_03/script.js
+++ common/js/xiaomi_03/script.js
@@ -32,20 +32,20 @@ _common.sendAjax(apiUrl, 'json', function(data) {
         product_list_06: solt_data.product_list_06
       }
     });
+    banner();
   }
 });
 
-function childrenEvent() {
-
-  //头图轮换
+/* 头图轮换 */
+function banner() {
   var index = 0;
-  var _time = '';
+  var _time = 0;
   var winHei = 0;
   var $items = $('.slider').find('.part');
 
   var change = function() {
     $items.removeClass('active').eq(index).addClass('active');
-  }
+  };
 
   var timer = function() {
     _time = setInterval(function() {
@@ -56,81 +56,67 @@ function childrenEvent() {
       }
       change();
     }, 3000);
-  }
+  };
 
   var setHead = function() {
-    winHei = $(window).height() - $('#J-nav').find('.items').height();
-  }
+    window.winHei = $(window).height() - $('#J-nav').find('.items').height();
+  };
+  timer();
   setHead();
-
-  //导航吸顶、跳转
-  var navEvent = function() {
-    var $nav = $('#J-nav');
-    var $items = $nav.find('.items');
-    var target = '';
-    var $scrollTop = $(window).scrollTop();
-
-    $(window).on('scroll', function() {
-      if ($(this).scrollTop() > winHei) {
-        $nav.addClass('fixed');
-      } else {
-        $nav.removeClass('fixed');
-      }
-    });
-
-    $items.on('click', function() {
-      $(this).addClass('active').siblings().removeClass('active');
-      target = $(this).attr('data-target');
-
-      if (target && $(target).length) {
-        $('html, body').animate({
-          scrollTop: $(target).offset().top
-        }, 500);
-      }
-      return false;
-    });
-  }
-
-  $(window).resize(function() {
-    setHead();
-  });
-
-  var init = function() {
-    navEvent();
-    timer();
-  }
-
-  init();
-
-  $('.share').on('click', function() {
-    share();
-  });
-
-  var shareContent = [{
-      title: '刚刚在小米网参加#小米儿童节专场#，看到了辣条、大白兔、弹珠、悠悠球！亲爱的小伙伴们，再次看到这些属于我们那个时代的零食玩具，真是感慨万分！这个“六一”，找回童年，像小时候一样快乐！分享还可以获得小米5尊享版（3D陶瓷）的优先购买资格噢！',
-      pic: 'http://hd.mi.com/y/05241o/img/childhood-weibo1.jpg'
-    },
-
-    {
-      title: '刚刚在小米网参加#小米儿童节专场#，看到了辣条、大白兔、弹珠、悠悠球！亲爱的小伙伴们，再次看到这些属于我们那个时代的零食玩具，真是感慨万分！这个“六一”，找回童年，像小时候一样快乐！分享还可以获得小米5尊享版（3D陶瓷）的优先购买资格噢！',
-      pic: 'http://hd.mi.com/y/05241o/img/childhood-weibo2.jpg'
-    }
-  ];
-
-  //分享
-  function share() {
-    var random = parseInt(Math.random() * 2);
-    var content = shareContent[random];
-    var url = 'http://hd.mi.com/y/05241o/index.html';
-    title = content.title;
-    pic = content.pic,
-      ralateUid = '',
-      language = '';
-    window.open('http://service.weibo.com/share/share.php?url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '&pic=' + encodeURIComponent(pic) + '&ralateUid=' + encodeURIComponent(ralateUid) + '&language=' + encodeURIComponent(language), '_blank', 'width=615,height=505');
-  }
 }
 
+/* 导航吸顶、跳转 */
+function navEvent() {
+  var $nav = $('#J-nav');
+  var $items = $nav.find('.items');
+  var target = '';
+  var $scrollTop = $(window).scrollTop();
 
-$(document).ready(function() {
-  childrenEvent();
+  $(window).on('scroll', function() {
+    if ($(this).scrollTop() > winHei) {
+      $nav.addClass('fixed');
+    } else {
+      $nav.removeClass('fixed');
+    }
+  });
+
+  $items.on('click', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    target = $(this).attr('data-target');
+
+    if (target && $(target).length) {
+      $('html, body').animate({
+        scrollTop: $(target).offset().top
+      }, 500);
+    }
+    return false;
+  });
+}
+navEvent();
+
+$('.share').on('click', function() {
+  share();
 });
+
+var shareContent = [{
+    title: '刚刚在小米网参加#小米儿童节专场#，看到了辣条、大白兔、弹珠、悠悠球！亲爱的小伙伴们，再次看到这些属于我们那个时代的零食玩具，真是感慨万分！这个“六一”，找回童年，像小时候一样快乐！分享还可以获得小米5尊享版（3D陶瓷）的优先购买资格噢！',
+    pic: 'http://hd.mi.com/y/05241o/img/childhood-weibo1.jpg'
+  },
+
+  {
+    title: '刚刚在小米网参加#小米儿童节专场#，看到了辣条、大白兔、弹珠、悠悠球！亲爱的小伙伴们，再次看到这些属于我们那个时代的零食玩具，真是感慨万分！这个“六一”，找回童年，像小时候一样快乐！分享还可以获得小米5尊享版（3D陶瓷）的优先购买资格噢！',
+    pic: 'http://hd.mi.com/y/05241o/img/childhood-weibo2.jpg'
+  }
+];
+
+//分享
+function share() {
+  var random = parseInt(Math.random() * 2);
+  var content = shareContent[random];
+  var url = 'http://hd.mi.com/y/05241o/index.html';
+  title = content.title;
+  pic = content.pic,
+    ralateUid = '',
+    language = '';
+  window.open('http://service.weibo.com/share/share.php?url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '&pic=' + encodeURIComponent(pic) + '&ralateUid=' + encodeURIComponent(ralateUid) + '&language=' + encodeURIComponent(language), '_blank', 'width=615,height=505');
+}
